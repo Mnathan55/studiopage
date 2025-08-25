34d77031c3c0a76d026e9fa36cc55daf89971cb8
ve-edant <vedantsalunke17473@gmail.com>
2025-08-26 02:30:21 +0530

    LoaderFix2

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,11 @@ import AboutUs from "./pages/AboutUs";
 import ScrollToTop from "./components/ScrollToTop";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
-import { useEffect, useState } from "react";
-import Loader from "./components/Loader";
 import SmoothScroll from "./lib/SmoothScroll";
 import Clients from "./pages/Clients";
 
 
 const App = () => {
-  const [showLoader, setShowLoader] = useState(true);
-
-  useEffect(() => {
-    const hasVisited = localStorage.getItem("hasVisited");
-
-    if (!hasVisited) {
-      // First time → show loader
-      setShowLoader(true);
-
-      // Set a flag so loader won't show again
-      localStorage.setItem("hasVisited", "true");
-    }
-    const timer = setTimeout(() => setShowLoader(false), 5000); // match GSAP animation duration
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (showLoader) {
-    return <Loader />; // only show loader (nothing else yet)
-  }
   return (
     <Router>
       <SmoothScroll>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,29 @@ import AboutQuick from '../components/Home/AboutQuick'
 import ServicesSection from '../components/Home/ServicesSection'
 import BenefitsSection from '../components/Home/BenefitsSection'
 import MoveForward from '../components/Home/MoveForward'
+import { useEffect, useState } from "react";
+import Loader from '../components/Loader'
 
 const Home = () => {
+  const [showLoader, setShowLoader] = useState(true);
+  
+    useEffect(() => {
+      const hasVisited = localStorage.getItem("hasVisited");
+  
+      if (!hasVisited) {
+        // First time → show loader
+        setShowLoader(true);
+  
+        // Set a flag so loader won't show again
+        localStorage.setItem("hasVisited", "true");
+      }
+      const timer = setTimeout(() => setShowLoader(false), 5000); // match GSAP animation duration
+      return () => clearTimeout(timer);
+    }, []);
+  
+    if (showLoader) {
+      return <Loader />; // only show loader (nothing else yet)
+    }
   return (
     <div>
         <HeroSection />
